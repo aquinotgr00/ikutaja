81782e96b625aeec462e1388a16ab976f58eab9b
aquinotgr <aquino@terascode.com>
2018-12-31 18:25:49 +0700

    adding Volunteer view controller and volunteer dashboard file

---
+++ application/views/pages/volunteer/pageDashboard.php
@@ -0,0 +1,17 @@
+<!--Breadcrumb Area Start-->
+<div class="breadcrumb-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="breadcrumb-content text-center">
+                    <h1 class="breadmome-name">Dashboard</h1>
+                    <ul>
+                        <li><a href="<?php echo base_url('/'); ?>">Home</a></li>
+                        <li class="active">Dashboard</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Breadcrumb Area End-->
