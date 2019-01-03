3cb70817ea2ec7872b0e729e3d113675ba00ee01
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2019-01-03 16:15:35 +0700

    minify all
    1. pages
    2. css
    3. js
    4. image

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -1,26 +1,20 @@
 <!doctype html>
-<html class="no-js" lang="en">
+<html class=no-js lang=en>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	
-	<title><?= $title ?></title>
-	
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<link rel="shortcut icon" type="image/x-icon" href="https://res.cloudinary.com/ikutaja/image/upload/v1546101683/img/fav/1.png">            
-
-	<link rel="stylesheet" href="https://use.typekit.net/gmm4qcx.css">    	
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">   		
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
-	
-	<link rel="stylesheet" href="https://res.cloudinary.com/ikutaja/raw/upload/v1546083522/css/plugins.min.css">	
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">	
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>">		
-	<link rel="stylesheet" href="https://res.cloudinary.com/ikutaja/raw/upload/v1546083614/css/responsive.min.css">	
-	
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css">	
-	<script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"></script>	
-	<link rel="stylesheet" href="https://res.cloudinary.com/ikutaja/raw/upload/v1546165228/css/map.css">
+<meta charset=utf-8>
+<meta http-equiv=x-ua-compatible content="ie=edge">
+<title><?= $title ?></title>
+<meta name=description content>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type=image/x-icon href=https://res.cloudinary.com/ikutaja/image/upload/v1546101683/img/fav/1.png>
+<link rel=stylesheet href=https://use.typekit.net/gmm4qcx.css>
+<link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css>
+<link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css>
+<link rel=stylesheet href=https://res.cloudinary.com/ikutaja/raw/upload/v1546083522/css/plugins.min.css>
+<link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css>
+<link rel=stylesheet href=<?php echo base_url('assets/css/main.css'); ?>>
+<link rel=stylesheet href=https://res.cloudinary.com/ikutaja/raw/upload/v1546083614/css/responsive.min.css>
+<link rel=stylesheet href=https://unpkg.com/leaflet@1.4.0/dist/leaflet.css>
+<script src=https://unpkg.com/leaflet@1.4.0/dist/leaflet.js></script>
+<link rel=stylesheet href=https://res.cloudinary.com/ikutaja/raw/upload/v1546165228/css/map.css>
 </head>
--- application/views/pages/public/pageTentang.php
+++ application/views/pages/public/pageTentang.php
@@ -1,61 +1,48 @@
-  		<!-- cabang -->
-          <div class="breadcrumb-area">
-		    <div class="container">
-		        <div class="row">
-		            <div class="col-12">
-		                <div class="breadcrumb-content text-center">
-                            <h1 class="breadmome-name">Tentang</h1>
-		                    <ul>
-		                        <li><a href="<?php echo base_url('about'); ?>">Home</a></li>
-		                        <li class="active">Tentang</li>
-		                    </ul>
-		                </div>
-		            </div>
-		        </div>
-		    </div>
-		</div>
-        <!-- cabang -->
-		                    
-        <!--Our Causes Area Start-->
-		<div class="about-area pt-50">
-		    <div class="container">
-		        <div class="row">
-		            <div class="col-12">
-		                <!--Section Title Start-->
-		                <div class="section-title">
-                            <h2>Tentang Ikutaja</h2>
-                            <p class="pb-20">Kami adalah sekelompok orang-orang biasa yang ingin menjadi bagian dari usaha dalam membuat Indonesia menjadi lebih baik.</p>                            
-		                    <p class="pb-30">Misi kami adalah untuk membuat kolaborasi antara relawan dan komunitas dengan misi yang hebat menjadi lebih mudah.</p>                            
-                            <a class="default-btn" href="<?php echo base_url('misi');?>">Misi Hebat Kami</a>		    
-		                </div>
-		                <!--Section Title End-->
-		            </div>
-		        </div>		    		        
-		    </div>
-		</div>
-        <!--Our Event Area End-->      
-        
-        <div class="about-area pt-50">
-		    <div class="container">
-		        <div class="row">	
-                    <div class="col-md-6 offset-md-3 center">
-		                <!--About Image Area Start-->
-		                <div class="about-img-area img-full">
-		                    <div class="img-title">
-		                        <h2>Ikutaja</h2>
-		                        <img draggable="false" src="https://res.cloudinary.com/ikutaja/image/upload/v1546101376/img/about/1.png" alt="">
-		                    </div>
-		                </div>
-		                <!--About Image Area End-->
-		            </div>	            
-		            <div class="col-md-6 offset-md-3 center pt-30 pb-50">
-		                <!--About Content Area Start-->
-		                <div class="section-title">		                    		                    
-							<a class="default-btn" href="<?php echo base_url('team');?>">Kenali Tim Ikutaja</a>
-		                </div>
-		                <!--About Content Area End-->
-		            </div>
-		        </div>
-		    </div>
-		</div>
-   
+<div class="breadcrumb-area">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="breadcrumb-content text-center">
+<h1 class="breadmome-name">Tentang</h1>
+<ul>
+<li><a href="<?php echo base_url('about'); ?>">Home</a></li>
+<li class="active">Tentang</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="about-area pt-50">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="section-title">
+<h2>Tentang Ikutaja</h2>
+<p class="pb-20">Kami adalah sekelompok orang-orang biasa yang ingin menjadi bagian dari usaha dalam membuat Indonesia menjadi lebih baik.</p>
+<p class="pb-30">Misi kami adalah untuk membuat kolaborasi antara relawan dan komunitas dengan misi yang hebat menjadi lebih mudah.</p>
+<a class="default-btn" href="<?php echo base_url('misi');?>">Misi Hebat Kami</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="about-area pt-50">
+<div class="container">
+<div class="row">
+<div class="col-md-6 offset-md-3 center">
+<div class="about-img-area img-full">
+<div class="img-title">
+<h2>Ikutaja</h2>
+<img draggable="false" src="https://res.cloudinary.com/ikutaja/image/upload/v1546492336/img/about/team.png" alt="">
+</div>
+</div>
+</div>
+<div class="col-md-6 offset-md-3 center pt-30 pb-50">
+<div class="section-title">
+<a class="default-btn" href="<?php echo base_url('teams');?>">Kenali Tim Ikutaja</a>
+</div>
+</div>
+</div>
+</div>
+</div>
