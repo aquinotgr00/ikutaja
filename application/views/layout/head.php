4f83df53a93d21b618a7cb75225748916846d490
aquinotgr <aquino@terascode.com>
2019-01-05 10:00:19 +0700

    styling notification alert on login

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -23,4 +23,6 @@
 	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css">	
 	<script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"></script>	
 	<link rel="stylesheet" href="https://res.cloudinary.com/ikutaja/raw/upload/v1546165228/css/map.css">
+
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
 </head>
