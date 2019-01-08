5d2b599ecbae6e9436b0210c42be21102927f134
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2019-01-08 22:10:25 +0700

    adding pageAdmin...

--- application/config/routes.php
+++ application/config/routes.php
@@ -6,6 +6,7 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['(:any)'] = 'Front/$1';
+$route['(:any)'] = 'admin/login/$1';
 
 /*
 | -------------------------------------------------------------------------
