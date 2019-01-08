9857d8b1a0a317d2ce5ac796f4da1e20d62dfb42
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2019-01-09 01:49:27 +0700

    edit and add errorPages | remove something in routes

--- application/views/errors/html/error_general.php
+++ application/views/errors/html/error_general.php
@@ -3,62 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Error</title>
-<style type="text/css">
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>404! Error</title>
+	<link rel="shortcut icon" type=image/x-icon href=https://res.cloudinary.com/ikutaja/image/upload/v1546101683/img/fav/1.png>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://res.cloudinary.com/ikutaja/raw/upload/v1546972667/404/main.css">       
+	
 </head>
+
+
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+ 
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>4<span></span>4</h1>
+			</div>
+			<h2>Oops! Terjadi Kesalahan</h2>
+			<p>Maaf karena ketidaknyamanannya, halaman sedang dalam pembangunan atau anda ingin <b>kembali ke</b></p>
+			<a href="http://ikutaja.id">HOMEPAGE</a>
+		</div>
 	</div>
+
 </body>
 </html>
