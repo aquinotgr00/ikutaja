7ffad1efcf058c64e8a0e49a9e14bf4c4ef5f1b6
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2019-01-09 23:13:44 +0700

    pageDashboard update(1)

--- application/controllers/Organizations.php
+++ application/controllers/Organizations.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Organizations extends CI_Controller {
 
 	public function dashboard() {
-		$data=array('title' => 'Ikutaja - Dashboard Organizations',
-          'page' => 'pages/relawan/pageDashboard',
+		$data=array('title' => 'Ikutaja - Dashboard Organization',
+          'page' => 'pages/pelaksana/pageDashboard',
           'authMiddleware' => true,
 					'xscript' => 'scripts/scriptOrganizationsDashboard');
 		$this->load->view('layout/wrapper', $data);
--- application/views/pages/pelaksana/pageDashboard.php
+++ application/views/pages/pelaksana/pageDashboard.php
@@ -79,6 +79,154 @@
                     </ul>
                 </div>
             </nav>
+                            <div class="table-stats order-table ov-h">
+                                <table class="table ">
+                                    <thead>
+                                        <tr>
+                                            <th class="serial">#</th>
+                                            <th class="avatar">Avatar</th>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Product</th>
+                                            <th>Quantity</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="serial">1.</td>
+                                            <td class="avatar">
+                                                <div class="round-img">
+                                                    <a href="#"><img class="rounded-circle" src="<?php echo base_url('assetsAdmin/img/avatar/1.jpg'); ?>" alt=""></a>
+                                                </div>
+                                            </td>
+                                            <td> #5469 </td>
+                                            <td>  <span class="name">Louis Stanley</span> </td>
+                                            <td> <span class="product">iMax</span> </td>
+                                            <td><span class="count">231</span></td>
+                                            <td>
+                                                <button type="button" class="btn btn-success mb-1" data-toggle="modal" data-target="#largeModal">
+                                                    EDIT
+                                                </button>
+                                                <button type="button" class="btn btn-danger mb-1">
+                                                    DELETE
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="serial">2.</td>
+                                            <td class="avatar">
+                                                <div class="round-img">
+                                                    <a href="#"><img class="rounded-circle" src="<?php echo base_url('assetsAdmin/img/avatar/2.jpg'); ?>" alt=""></a>
+                                                </div>
+                                            </td>
+                                            <td> #5468 </td>
+                                            <td>  <span class="name">Gregory Dixon</span> </td>
+                                            <td> <span class="product">iPad</span> </td>
+                                            <td><span class="count">250</span></td>
+                                            <td>
+                                                <button type="button" class="btn btn-success mb-1" data-toggle="modal" data-target="#largeModal">
+                                                    EDIT
+                                                </button>
+                                                <button type="button" class="btn btn-danger mb-1">
+                                                    DELETE
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="serial">3.</td>
+                                            <td class="avatar">
+                                                <div class="round-img">
+                                                    <a href="#"><img class="rounded-circle" src="<?php echo base_url('assetsAdmin/img/avatar/3.jpg'); ?>" alt=""></a>
+                                                </div>
+                                            </td>
+                                            <td> #5467 </td>
+                                            <td>  <span class="name">Catherine Dixon</span> </td>
+                                            <td> <span class="product">SSD</span> </td>
+                                            <td><span class="count">250</span></td>
+                                            <td>
+                                                <button type="button" class="btn btn-success mb-1" data-toggle="modal" data-target="#largeModal">
+                                                    EDIT
+                                                </button>
+                                                <button type="button" class="btn btn-danger mb-1">
+                                                    DELETE
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="serial">4.</td>
+                                            <td class="avatar">
+                                                <div class="round-img">
+                                                    <a href="#"><img class="rounded-circle" src="<?php echo base_url('assetsAdmin/img/avatar/4.jpg'); ?>" alt=""></a>
+                                                </div>
+                                            </td>
+                                            <td> #5466 </td>
+                                            <td>  <span class="name">Mary Silva</span> </td>
+                                            <td> <span class="product">Magic Mouse</span> </td>
+                                            <td><span class="count">250</span></td>
+                                            <td>
+                                                <button type="button" class="btn btn-success mb-1" data-toggle="modal" data-target="#largeModal">
+                                                    EDIT
+                                                </button>
+                                                <button type="button" class="btn btn-danger mb-1">
+                                                    DELETE
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div> <!-- /.table-stats -->
+
+                            <div class="modal fade pt-120" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="largeModalLabel">Large Modal</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="#" method="post" enctype="multipart/form-data" class="form-horizontal">
+                                                <div class="row form-group">
+                                                    <div class="col col-md-3"><label class=" form-control-label">Static</label></div>
+                                                    <div class="col-12 col-md-9">
+                                                        <p class="form-control-static">Username</p>
+                                                    </div>
+                                                </div>
+                                                <div class="row form-group">
+                                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Text Input</label></div>
+                                                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="text-input" placeholder="Text" class="form-control"><small class="form-text text-muted">This is a help text</small></div>
+                                                </div>
+                                                <div class="row form-group">
+                                                    <div class="col col-md-3"><label for="email-input" class=" form-control-label">Email Input</label></div>
+                                                    <div class="col-12 col-md-9"><input type="email" id="email-input" name="email-input" placeholder="Enter Email" class="form-control"><small class="help-block form-text">Please enter your email</small></div>
+                                                </div>
+                                                <div class="row form-group">
+                                                    <div class="col col-md-3"><label for="password-input" class=" form-control-label">Password</label></div>
+                                                    <div class="col-12 col-md-9"><input type="password" id="password-input" name="password-input" placeholder="Password" class="form-control"><small class="help-block form-text">Please enter a complex password</small></div>
+                                                </div>                                                                                                                                                 
+                                                <div class="row form-group">
+                                                    <div class="col col-md-3"><label for="select" class=" form-control-label">Select</label></div>
+                                                    <div class="col-12 col-md-9">
+                                                        <select name="select" id="select" class="form-control">
+                                                            <option value="0">Please select</option>
+                                                            <option value="1">Option #1</option>
+                                                            <option value="2">Option #2</option>
+                                                            <option value="3">Option #3</option>
+                                                        </select>
+                                                    </div>
+                                                </div>                                                
+                                            </form>    
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                            <button type="button" class="btn btn-primary">Confirm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            
         </div>
     </div>
 </div>
